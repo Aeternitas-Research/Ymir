ddd829898fbb64e17e88184cc8cb95a2c23e35a4
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2026-03-19 19:26:33 -0400

    feat: add logging

--- Ymir/simulation/gkeyll/simulation.py
+++ Ymir/simulation/gkeyll/simulation.py
@@ -1,9 +1,17 @@
+import logging
+
+
 class Simulation:
     def __init__(self, config):
+        self.logger = logging.getLogger(__name__)
         self.config = config
 
+        self.logger.info("Simulation `gkeyll` initialized")
+
     def start(self):
-        pass
+        self.logger.info("START: Simulation:gkeyll")
+
+        self.logger.info("STOP: Simulation:gkeyll")
 
 
 __all__ = ["Simulation"]
